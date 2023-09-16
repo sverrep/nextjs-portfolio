211b20af223e4800bc931141d686b413e04261e0
sverrep <sverre.paulsen@code.berlin>
2023-09-16 13:14:27 +0200

    About Description and New Resume

--- components/About.jsx
+++ components/About.jsx
@@ -13,33 +13,36 @@ const About = () => {
       </div>
 
       <div className="lg:grid lg:h-full lg:pb-20 min-h-fit w-full lg:grid-cols-8 xl:gap-16 bg-primaryLight/40 -mt-8 lg:-mt-20 -mb-32 pt-10">
-        <div className="col-span-4 2xl:col-span-3 flex flex-col items-start justify-start px-10 lg:px-20 xl:px-36 pt-5 lg:pt-14 text-lg">
+        <div className="col-span-4 2xl:col-span-3 flex flex-col items-start justify-start px-10 lg:pl-20 xl:pl-24 pt-5 lg:pt-14 text-lg xl:pt-20 2xl:text-xl">
           <p className="font-medium">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisi
-            scelerisque eu ultrices vitae.
+            As a Norwegian who grew up in Spain attending an international
+            school, I've been exposed to a diverse range of perspectives and
+            cultures from a young age. As a result, I learned how to be
+            adaptable and have an open mind early on, which has continuously
+            proven to be invaluable to me as a software engineer.
           </p>
           <p className="my-4 font-medium">
-            Pellentesque habitant morbi tristique senectus et netus et. Rhoncus
-            urna neque viverra justo nec ultrices dui. Tempor orci eu lobortis
-            elementum nibh tellus. Eu volutpat odio facilisis mauris sit. Platea
-            dictumst quisque sagittis purus sit. Sed adipiscing diam donec
-            adipiscing tristique risus nec. Ac felis donec et odio pellentesque
-            diam.
+            Although I so far have limited professional experience, I'm eager to
+            learn and have already worked on several projects that have allowed
+            me to develop and hone my skills with multiple languages, such as
+            Javascript and Python, and frameworks, like React, Next.js, and
+            Django.
           </p>
           <p className="font-medium">
-            Nulla facilisi nullam vehicula ipsum a arcu cursus. Arcu non sodales
-            neque sodales ut etiam sit amet nisl. Amet venenatis urna cursus
-            eget nunc scelerisque. Eu facilisis sed odio morbi quis commodo odio
-            aenean.
+            Ultimately, I'm driven by a desire to develop innovative and
+            effective solutions to complex problems. I'm excited about the
+            chance to network with others in the industry and explore new
+            opportunities, so please don't hesitate to reach out.
           </p>
         </div>
         <div className="col-span-4 2xl:col-span-5 items-center pb-24 pt-10 lg:pt-0 lg:pb-0">
-          <Header text="Skillset" className="text-center font-bold text-3xl text-primary uppercase"/>
+          <Header
+            text="Skillset"
+            className="text-center font-bold text-3xl text-primary uppercase"
+          />
           <Skills />
         </div>
       </div>
-
     </div>
   );
 };
